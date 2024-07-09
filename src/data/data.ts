eeee3a8ed01ea6497e282dd3d00880c8c263bf9f
rhyanmiranda <mirandarhyan90@gmail.com>
2024-07-09 13:19:29 -0300

    created project

---
+++ src/data/data.ts
@@ -0,0 +1,22 @@
+export const movies = [
+  { id: 1 , name: 'Batman: O Cavaleiro das Trevas', anoLancamento: 2008, ondeAssistir: 'Prime Video' },
+  { id: 2 , name: 'Interestelar', anoLancamento: 2014, ondeAssistir: 'Prime Video' },
+  { id: 3 , name: 'O Poderoso Chefão', anoLancamento: 1972, ondeAssistir: 'Prime Video' },
+  { id: 4 , name: 'Pulp Fiction: Tempo de Violência', anoLancamento: 1994, ondeAssistir: 'Prime Video' },
+  { id: 5 , name: 'Matrix', anoLancamento: 1999, ondeAssistir: 'Prime Video' },
+  { id: 6 , name: 'Titanic', anoLancamento: 1997, ondeAssistir: 'Prime Video' },
+  { id: 7 , name: 'Vingadores: Ultimato', anoLancamento: 2019, ondeAssistir: 'Disney+' },
+  { id: 8 , name: 'A Origem', anoLancamento: 2010, ondeAssistir: 'Prime Video' },
+  { id: 9 , name: 'Star Wars: Episódio IV - Uma Nova Esperança', anoLancamento: 1977, ondeAssistir: 'Disney+' },
+  { id: 10 , name: 'Jurassic Park: O Parque dos Dinossauros', anoLancamento: 1993, ondeAssistir: 'Prime Video' },
+  { id: 11 , name: 'O Rei Leão', anoLancamento: 1994, ondeAssistir: 'Disney+' },
+  { id: 12 , name: 'Harry Potter e a Pedra Filosofal', anoLancamento: 2001, ondeAssistir: 'Prime Video' }
+]
+
+export const streamers = [
+   'Star+',
+   'Paramount+',
+   'AppleTV',
+   'Prime Video',
+   'Disney+',
+]
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import fastify from "fastify";
+import { movies, streamers } from "./data/data";
+
+const app = fastify()
+
+app.get('/movies', (request, reply) => {
+  reply.type('application/json').code(200)
+  return {
+    message: 'List Movies',
+    movies: movies
+  }
+})
+
+app.get('/streamers', (request, reply) => {
+  reply.type('application/json').code(200)
+  return {
+    message: 'List Streamers',
+    streamers: streamers
+  }
+})
+
+interface ReplyStreamerModel {
+  name: string
+}
+
+app.get<{ Params: ReplyStreamerModel }>('/movies/streamers/:name', (request, reply) => {
+  const streamer = request.params.name
+  // let filmesPorStreamer = []
+  const filteredMovies = movies.filter(
+    movie => movie.ondeAssistir.toLowerCase() === streamer.toLowerCase()
+  )
+  return filteredMovies
+})
+
+app.listen({
+  port: 3000
+}).then(()=> console.log('server init'))
+
